b6d3e74258941fe8b2c96dc77b753c1f753ec406
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-03-11 13:54:59 +0100

    fix: gradle build

--- app/src/main/java/pcd/lab03/cswithlocks/TestCSWithLocks.java
+++ app/src/main/java/pcd/lab03/cswithlocks/TestCSWithLocks.java
@@ -6,8 +6,7 @@ public class TestCSWithLocks {
 	public static void main(String[] args) {
 		Lock lock = new ReentrantLock();
 		new MyWorkerB("MyAgent-01", lock).start();
-		new MyWorkerA("MyAgent-02", lock).start();		
-	
+		new MyWorkerA("MyAgent-02", lock).start();
 	}
 
 }
